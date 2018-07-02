d9ab5003332145449f544f028a465a6f93f73488
DunnCreativeSS <jarett+github@dcssquared.com>
2018-07-02 18:30:04 -0300

    40

--- BFXfibbmonster.js
+++ BFXfibbmonster.js
@@ -1165,7 +1165,7 @@ async function setBal(){
 	console.log(btcusd);
 	rest.calcAvailableBalance('tBTCUSD', 1, btcusd, 'MARGIN').then(balances => {
 	var btcusdavail = (balances[0] * btcusd);
-	divisor = btcusdavail / 19
+	divisor = btcusdavail / 40
 	console.log('divisor: ' + divisor);
 	if (divisor <= .8){
 		godosell = false;
