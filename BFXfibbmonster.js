30f2d5fb4efcff1034f0056818d18b28b61057a6
Jarett Dunn <jarettrsdunn@gmail.com>
2018-07-08 06:53:58 -0300

    lala

--- BFXfibbmonster.js
+++ BFXfibbmonster.js
@@ -1193,9 +1193,9 @@ async function setBal(){
 	rest.calcAvailableBalance('tETHUSD', 1, ethusd, 'MARGIN').then(balances => {
 	var btcusdavail = (balances[0] * ethusd);
 	console.log(btcusdavail);
-	divisor = btcusdavail / 100
+	divisor = btcusdavail / 50
 	console.log('divisor: ' + divisor);
-	if (divisor <= .8){
+	if (divisor <= 1.5){
 		godosell = false;
 		godobuy = false;
 		console.log('NONO buy buy! NONO sell sell!');
